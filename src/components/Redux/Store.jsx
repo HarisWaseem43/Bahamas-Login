59c32ba0f659fc8cec31fcae1c95f2b35854d171
Haris <hariswaseem143@gmail.com>
2023-09-11 11:04:53 +0500

    5th commit

--- src/components/Redux/Store.jsx
+++ src/components/Redux/Store.jsx
@@ -1,12 +1,9 @@
-// store.js
-
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./authSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
-    // Add other reducers if needed
   },
 });
 
